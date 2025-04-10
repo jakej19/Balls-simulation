abbe18c1cc776aca8e1ab4be7ce94ff8f744f73c
Jake <hcsg34@durham.ac.uk>
2025-04-10 02:42:40 +0100

    Added musical functionality with fireflies song

--- main.py
+++ main.py
@@ -11,8 +11,20 @@ import math
 import pymunk
 import pymunk.pygame_util
 import random
+import pygame.midi
+from utils.music import NOTES
+import time
+import numpy as np
+
+song_file = "songs/fireflies.txt"
+with open(song_file, "r") as f:
+    song = f.read().split(",")
+note_state = {"note_no": 0}
 
 pygame.init()
+pygame.midi.init()
+midi_out = pygame.midi.Output(0)
+midi_out.set_instrument(9)
 # Screen properties
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
@@ -22,13 +34,20 @@ clock = pygame.time.Clock()
 FPS = 60
 background_color = (30, 30, 30)
 
+
+BALL_COLLISION_TYPE = 1
+STATIC_COLLISION_TYPE = 2
 # Constants
-g = 500
+g = 50
 restitution = 1.01
 physics_steps_per_frame = 10
 space = pymunk.Space()
 space.gravity = (0, g)
 space.iterations = 30
+
+
+def get_midi(note):
+    return NOTES[note]
 
 
 class Ball:
@@ -47,6 +66,7 @@ class Ball:
         self.shape.elasticity = restitution
         self.shape.friction = 0
 
+        self.shape.collision_type = BALL_COLLISION_TYPE
         space.add(self.body, self.shape)
 
     def draw(self, screen):
@@ -69,13 +89,28 @@ def create_container(space, pos, radius, num_segments=32, thickness=3, elasticit
         segment.elasticity = elasticity
         segment.friction = 0
         segments.append(segment)
+        segment.collision_type = STATIC_COLLISION_TYPE
         space.add(segment)
+
     return segments
+
+
+def on_ball_static_collision(arbiter, space, data):
+    global note_no
+    note = get_next_note()
+    velocity = 127
+    midi_out.note_on(note, velocity)
+    return True
 
 
 container_centre = pymunk.Vec2d(width / 2, height / 2)
 container_radius = 280
 container_segments = create_container(space, container_centre, container_radius)
+
+
+handler = space.add_collision_handler(BALL_COLLISION_TYPE, STATIC_COLLISION_TYPE)
+handler.begin = on_ball_static_collision
+
 
 """possible_colors = {
     "Violet": (144, 0, 211),
@@ -97,7 +132,6 @@ possible_colors = [
 ]
 unused_colors = possible_colors.copy()
 
-
 def draw_container(screen, pos, radius, color=(155, 155, 155), line_width=2):
     pygame.draw.circle(screen, color, (int(pos.x), int(pos.y)), radius, line_width)
 
@@ -105,9 +139,19 @@ def draw_container(screen, pos, radius, color=(155, 155, 155), line_width=2):
 def get_rand_color(colors):
     if not colors:
         colors = possible_colors.copy()
-    color = random.choice(colors)
+        
+    color = np.array(random.choice(colors))
     colors.remove(color)
+    color += np.random.randint(-30, 30, 3)
+    color = color.clip(0, 255)
+
     return color
+
+
+def get_next_note():
+    note = get_midi(song[note_state["note_no"]])
+    note_state["note_no"] += 1
+    return note
 
 
 balls = [
@@ -116,21 +160,21 @@ balls = [
         pos=(400, 100),
         vel=(0, 0),
         radius=10,
-        color=get_rand_color(unused_colors),
+        color=get_rand_color(possible_colors),
     ),
     Ball(
         space,
         pos=(300, 100),
         vel=(0, 0),
         radius=10,
-        color=get_rand_color(unused_colors),
+        color=get_rand_color(possible_colors),
     ),
     Ball(
         space,
-        pos=(500, 100),
+        pos=(500, 101),
         vel=(0, 0),
         radius=10,
-        color=get_rand_color(unused_colors),
+        color=get_rand_color(possible_colors),
     ),
 ]
 
@@ -148,7 +192,6 @@ while running:
     for _ in range(physics_steps_per_frame):
         space.step(sub_dt)
 
-
     screen.fill(background_color)
     draw_container(screen, container_centre, container_radius)
     for ball in balls:
@@ -156,5 +199,7 @@ while running:
 
     pygame.display.flip()
 
+del midi_out
+pygame.midi.quit()
 pygame.quit()
 sys.exit()
